392e174df9e6de39d3745412bcb25b4e12b64d61
Diego AM <diegoabm.dev@gmail.com>
2026-03-09 18:32:26 -0300

    feat: configuracion inicial, uso variables .env en docker-compose y .env ejemplo

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Prefijo para la API (ej: localhost:3000/api/v1/...)
+  app.setGlobalPrefix('api/v1');
+
+  // Configuración de validaciones globales (DTOs)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
