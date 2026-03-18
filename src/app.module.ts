f33b110642bcdcf67371deb8e03db07fa3c76c0e
Diego AM <diegoabm.dev@gmail.com>
2026-03-17 22:48:10 -0300

    changes for render

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,12 +23,16 @@ import { ReviewsModule } from './modules/reviews/reviews.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        synchronize: true,
+        synchronize: configService.get<string>('STATE') !== 'prod',
+        ssl:
+          configService.get<string>('STATE') === 'prod'
+            ? { rejectUnauthorized: false }
+            : false,
       }),
     }),
 
